63b7f9c2c3c9f3920b91e0cd24a79ddf391dc0c5
tommylee9068 <tommylee9068@gmail.com>
2026-08-27 14:18:28 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
@@ -0,0 +1,10 @@
+def solution(numbers, n):
+    answer = 0
+    for i in range(len(numbers)):
+        answer += numbers[i]
+        if answer > n:
+            return answer 
+    
+    
+    # answer = 0
+    # return answer

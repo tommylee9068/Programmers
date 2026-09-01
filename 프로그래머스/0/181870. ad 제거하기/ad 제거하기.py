47fb064f01236990dc8850d27511fce72ec34cdc
tommylee9068 <tommylee9068@gmail.com>
2026-09-01 09:42:36 +0900

    [level 0] Title: ad 제거하기, Time: 0.05 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,9 @@
+def solution(strArr):
+    answer = []
+    for i in strArr:
+        if 'ad' not in i:
+            answer.append(i)
+    return answer
+    
+    # answer = []
+    # return answer

509f8202eae60c41e4244b474f18606db659a4bc
tommylee9068 <tommylee9068@gmail.com>
2026-09-02 12:40:02 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -0,0 +1,11 @@
+def solution(rny_string):
+    answer = ''
+    for i in rny_string:
+        if i == "m":
+            answer  += 'rn'
+        else:
+            answer += i
+    return answer
+        
+    #answer = ''
+    #return answer

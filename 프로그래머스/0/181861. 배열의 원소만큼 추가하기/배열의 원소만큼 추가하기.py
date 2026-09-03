894a48fb234b9ff152608d1c718566189290da66
tommylee9068 <tommylee9068@gmail.com>
2026-09-03 14:26:11 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.20 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    for i in arr:
+        for a in range(i):
+            answer.append(i)
+    return answer
+            
+    
+    # answer = []
+    # return answer
